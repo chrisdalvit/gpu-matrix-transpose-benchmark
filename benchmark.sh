dec09c78e03028ffad72413ed7760fe55621c6d7
Christian Dalvit <chris.dalvit@gmail.com>
2024-04-15 21:05:41 +0200

    adjust benchmark file

--- benchmark.sh
+++ benchmark.sh
@@ -5,13 +5,13 @@ STATS_DIR=$2
 STATS_FILE=$STATS_DIR/$(date +'%y_%m_%dT%H:%M:%S')
 OPTIM=''
 
-echo name,optim,size,time >> $STATS_FILE;
+echo name,optim,size,time,bandwidth >> $STATS_FILE;
 
 for file in $(ls $BIN_DIR)
 do
     OPTIM=${file#*-} # Get optimization flag from suffix
     NAME=${file%-*} # Get file name
-    for size in $(seq 5 10) # Loop over sizes
+    for size in $(seq 8 13) # Loop over sizes
     do
         for i in $(seq 1 10) # Loop over the number of repetitions
         do
